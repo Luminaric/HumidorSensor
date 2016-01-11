d7209c37eca0bfa145369b85c1399f8836d54ed1
Luminaric <rekraus@gmail.com>
2016-01-11 16:54:42 -0500

    Create ethernetSettings.h

---
+++ ethernetSettings.h
@@ -0,0 +1,7 @@
+const char* ssid     = "SSID";
+const char* password = "password";
+IPAddress ip(xxx,xxx,xxx,xxx);
+IPAddress gateway(xxx,xxx,xxx,xxx);
+IPAddress subnet(255,255,255,0);
+const char* serverLH = "xxx,xxx,xxx,xxx";
+const char* serverTS = "184.106.153.149";
