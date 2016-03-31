0302d9ed356721d0b2b60e53a3bf595a087d065b
Luminaric <rekraus@gmail.com>
2016-03-31 11:12:33 -0400

    Changed ethernetSettings to reflect new library

--- ethernetSettings.h
+++ ethernetSettings.h
@@ -1,9 +1,3 @@
-String THING_SPEAK_KEY  =   "KEYVALUE";
-
 const char* ssid     = "SSID";
 const char* password = "password";
-IPAddress ip(xxx,xxx,xxx,xxx);
-IPAddress gateway(xxx,xxx,xxx,xxx);
-IPAddress subnet(255,255,255,0);
-const char* serverLH = "xxx,xxx,xxx,xxx";
-const char* serverTS = "184.106.153.149";
+IPAddress server(xxx,xxx,xxx,xxx);
