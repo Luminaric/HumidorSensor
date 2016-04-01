adb388b5f7be31e119007201c338c27c98541952
Luminaric <rekraus@gmail.com>
2016-03-31 21:01:26 -0400

    This was a major change to the code.  I decided to adopt the MQTT method for getting data.  This allows me to push the data to multiple destinations without having to reprogram the sensor each time. Once the sensor writes to the Mosquitto Broker a python script moves the data to the desired desitnation.

--- Humidor.ino
+++ Humidor.ino
@@ -1,152 +1,149 @@
 /*
- * This sketch writes directly on the ESP chip.
- * The ESP8266WiFi.h library is used here.
- * This sensor is battery powered and uses the ESP12 
- * chip. The chip is put into sleep between transmissions.
- * The DHT22 is used to get the Temp and Humidity. Battery 
- * voltage is also sampled and transmitted to the MySQL server.
- * The Temp and Humidity are also sent to ThingSpeak.
- * 
- * Version:   1.2
- * SensorID:  HUMIDOR
- * Platform:  ESP8266
- * Date:      2016-01-22
- * Requires:
- * Hardware:  Uses the DHT22 Temp and Humid sensor.
+  This sketch writes directly on the ESP chip.
+  The ESP8266WiFi.h library is used here.
+  This sensor is battery powered and uses the ESP12 
+  chip. The chip is put into sleep between transmissions.
+  The DHT22 is used to get the Temp and Humidity. Battery 
+  voltage is also sampled and transmitted to the MQTT server.
+  
+  Version:   2.0
+  SensorID:  HUMIDOR
+  Platform:  ESP8266
+  Date:      2016-03-17
+  Requires:
+  Hardware:  Uses the DHT22 Temp and Humid sensor. 
+
+  Update history
+  2016-03-17
+  Totally rewritten to use MQTT.
+  2016-03-30
+  Modified the DHT.cpp file.  Increased the delay after
+  setting pinMode to 40 from 10
  */
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include "ethernetSettings.h"
 #include <DHT.h>
+#include <PubSubClient.h>
 
 extern "C" {
 #include "user_interface.h"
 uint16 readvdd33(void);
 }
-#define THING_SPEAK1          "POST /update HTTP/1.1\n\rHost: api.thingspeak.com\n\rConnection:close\n\rX-THINGSPEAKAPIKEY: "
-#define THING_SPEAK2          "\n\rContent-type: application/x-www-form-urlencoded\n\rContent-Length: "
-#define MYSQL_OPEN_STRING     "Get /add2.php?f0="//this is the URL for the PI server
-#define CLOSE_STRING          "Host: 10.1.1.25 Connection: close"
-#define RESET_PIN             16
 #define DHTPIN                2
 #define DHTTYPE               DHT22
-#define SENSORID              "HUMIDOR"//change as required
+#define SENSORID              "HUMIDOR1"//change as required
 
-uint16 x;
-float t, h;
-float v = readvdd33() / 1000.0;
-String getString;
-const unsigned long sleepTimeS = 3600;//in seconds 3600
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient wireless;
+PubSubClient client(wireless);
+
+const char eStr[50] = " returned a invalid result";
+const unsigned long sleepTimeS = 3600;//60min = 3600, 30min = 1800
 const unsigned long multiplier = 1000000;//cycles for one second
+char voltage[7],humidity[7],temperatureC[7];
+float h,t;
+bool dataFlag;
 
-DHT dht(DHTPIN, DHTTYPE, 16);
-//
-WiFiClient client;
-
-void sendThingSpeak() {
-  //make the IOT string
-  String getIOTString = "field1=";
-  getIOTString += SENSORID;
-  getIOTString += "&field2=";
-  getIOTString += t*1.8+32;
-  getIOTString += "&field3=";
-  getIOTString += h;
-  getIOTString += "&field4=";
-  getIOTString += v;
-  Serial.print("iotString: ");
-  Serial.println(getIOTString);
-  //Send the data to ThingSpeak
-  if (client.connect(serverTS, 80))  { 
-      client.print(THING_SPEAK1);
-      client.print(THING_SPEAK_KEY);
-      client.print(THING_SPEAK2);
-      client.print(getIOTString.length());
-      client.print("\n\n");
-      client.print(getIOTString);
-      client.stop();
-  
-    }
+void getData () {
+  h = dht.readHumidity();
+  t = dht.readTemperature(); 
+  if (!isnan(t) || !isnan(h)) {   
+    Serial.print(h);Serial.print(" | ");Serial.println(t);
+    dtostrf(h,4,1,humidity);
+    dtostrf(t,4,1,temperatureC);
+    dtostrf(readvdd33()/1000.0,4,2,voltage);
+    dataFlag = true;
+    return;
   }
-  void getData(void) {
-    h = dht.readHumidity();
-    t = dht.readTemperature();
-    if (isnan(h) || isnan(t)) {
-      Serial.println("Failed to read from DHT Sensor");
-      /*some sort of function to log the 
-       * error should exist here */
-       ESP.restart();
-    }
-    Serial.print("Temp ");Serial.print(t);Serial.print("|");Serial.print("Humidity ");Serial.println(h);
-  }
-  void sendMySQL(void)  {
-    getString = MYSQL_OPEN_STRING;
-    getString += SENSORID;
-    getString += "&f1=";
-    getString += t;
-    getString += "&f2=";
-    getString += h;
-    getString += "&f3=";
-    getString += 0x00;
-    getString += "&f4=";
-    getString += v;
-    Serial.println(getString);
-    client.println(getString);
-    client.println(CLOSE_STRING);
-    client.println();
-    client.println();
-    client.stop();
-  }
-void  dhtTest() {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  float f = dht.readTemperature(true);
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
-    for(;;);
-  }
+  dataFlag = false;
+  //String eString ="Sensor had problems";
+  //eString.toCharArray(error, 100);
+  //humidity="0.00";temperature="0.00";
 }
-
 void setup() {
-  //Init DHT Sensor
-  dht.begin();
-  //Setup Serial and report status
+  /*--------------------------------------*/
+  /*      Send Sensor Info to Serial      */
+  /*--------------------------------------*/
   Serial.begin(9600);
-  //dhtTest();
-  Serial.print("DHT using PIN: ");
-  Serial.println(DHTPIN);
-  //Connect Wi-Fi
+  Serial.print(SENSORID);
+  Serial.print(" is using Pin ");
+  Serial.print(DHTPIN);
+  Serial.println(" For DHT sensor.");
+  delay(2000);//added this 3/24 to see if a delay gives the sensor
+              //a chance to get a good reading after recovering
+              //from uP deep sleep.
+  dht.begin();
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    x += 1;
-    //need to add logic here in case the ESP fails to connect to
-    //Wi-Fi
   }
   Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.println("WiFi connected");  
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  // Use WiFiClient class to create TCP connections
-  if (!client.connect(serverLH, 80)) {
-    Serial.println("connection failed");
-   return;
-  } else  {
-    getData();
-    sendMySQL();
-    sendThingSpeak(); 
+  client.setServer(server,1883);
+  /*--------------------------------------*/
+  /*                 Get Data             */
+  /*--------------------------------------*/
+  getData();
+   
+  /*--------------------------------------*/
+  /*            Connect to MQTT           */
+  /*--------------------------------------*/  
+  if ( dataFlag == true)  {
+  if(!client.connected()) {
+    client.connect(SENSORID);
+    Serial.println("Connected");
+    Serial.print("rc=");
+    Serial.println(client.state());
   }
-  
+  /*--------------------------------------*/
+  /*             Send to MQTT             */
+  /*--------------------------------------*/
+  String mqttString = "[\"";
+  mqttString += SENSORID;
+  mqttString += "\",{\"temperature\":\"";
+  mqttString += temperatureC;
+  mqttString += "\",\"humidity\":\"";
+  mqttString += humidity;
+  mqttString += "\",\"voltage\":\"";
+  mqttString += voltage;
+  mqttString += "\"}]";
+  Serial.print("MQTT String: ");
+  Serial.println(mqttString);
+  char payload[100];
+  mqttString.toCharArray(payload, 100);
+  client.publish("Sensors",payload);
+  char pload[50];
+  String logString = "Sensor ";
+  logString += SENSORID;
+  logString += " Trasmission Successful";
+  logString.toCharArray(pload, 50);
+  client.publish("Log",pload);
+  } else if (dataFlag == false) {
+      if(!client.connected()) {
+        client.connect(SENSORID);
+        Serial.println("Connected");
+        Serial.print("rc=");
+        Serial.println(client.state());
+      }
+      char pload[50];
+      String logString = "Sensor ";
+      logString += SENSORID;
+      logString += eStr;
+      logString.toCharArray(pload, 50);
+      client.publish("Log", pload);
+  }
   delay(1000);
-  system_deep_sleep_set_option(0);
-  system_deep_sleep((sleepTimeS * multiplier) - micros());
-
-
+//  system_deep_sleep_set_option(0);
+//  system_deep_sleep((sleepTimeS * multiplier) - micros());
+  ESP.deepSleep(sleepTimeS * multiplier);
 }
 
 void loop() {
-  delay(6000);
-
+  delay (10000);
+l
 }
-
